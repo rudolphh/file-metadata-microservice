6c09f11899f37c0c38dbdf2009aba77b7833129e
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-05-09 00:31:46 -0700

    Add error handling middleware taken from exercise api tracker project

--- server.js
+++ server.js
@@ -32,6 +32,29 @@ app.post('/api/fileanalyse', upload.single('upfile'), function(req, res, next) {
   res.json(fileInfo);
 });
 
+/////////////////// Taken from exercise tracker microservice api project
+
+// Not found middleware
+app.use((req, res, next) => next({status: 404, message: 'not found'}) );
+
+// Error Handling middleware
+app.use((err, req, res, next) => {
+  let errCode, errMessage
+
+  if (err.errors) {
+    // mongoose validation error
+    errCode = 400 // bad request
+    const keys = Object.keys(err.errors)
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message
+  } else {
+    // generic or custom error
+    errCode = err.status || 500
+    errMessage = err.message || 'Internal Server Error'
+  }
+  res.status(errCode).type('txt')
+    .send(errMessage)
+})
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
